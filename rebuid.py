d73efa5ac4a242b372057740651353cd13433153
gammapopolam <ioannaztecas@gmail.com>
2024-02-24 13:09:01 +0300

    implement simple spatial index

--- rebuid.py
+++ rebuid.py
@@ -1,5 +1,5 @@
 
-from shapely import from_geojson, intersects, equals, touches, relate
+from shapely import intersects, relate
 from shapely.geometry import MultiLineString, LineString
 #from shapely.ops import unary_union
 from shapely import unary_union, normalize
@@ -55,11 +55,14 @@ class Restrictions:
             return True
         else:
             return False
+#TODO: need more tests with DE-9IM
     def is_stream(self):
-        if relate(self.l1, self.l2)=='FF1F00102' and angle_checker(self.l1, self.l2)=='hallway':
+        if relate(self.l1, self.l2)=='FF1FF0102' and angle_checker(self.l1, self.l2)=='hallway':
             return True
-        elif relate(self.l1, self.l2)=='FF1FF0102' and angle_checker(self.l1, self.l2)=='hallway':
-            return True
+        #elif relate(self.l1, self.l2)=='FF1F00102' and angle_checker(self.l1, self.l2)=='hallway':
+        #    return True
+        #elif relate(self.l1, self.l2)=='FF1F0F102' and angle_checker(self.l1, self.l2)=='hallway':
+        #    return True
         else:
             return False
     def is_intersection(self):
@@ -73,27 +76,57 @@ class Restrictions:
 
 #TODO: update with multilinestrings, avoid using only first geom
 def angle_checker(l1, l2):
-    if l1.geom_type=='MultiLineString':
-        l1=list(l1.geoms[0].coords)
+    flag='undefined'
+    if l1.geom_type=='MultiLineString' and l2.geom_type=='MultiLineString':
+        l1_geoms=list(l1.geoms)
+        l2_geoms=list(l2.geoms)
+        for geom in l1_geoms:
+            l1=geom.coords
+            for geom2 in l2_geoms:
+                l2=geom2.coords
+                m1 = (l1[1][1]-l1[0][1])/(l1[1][0]-l1[0][0])
+                m2 = (l2[1][1]-l2[0][1])/(l2[1][0]-l2[0][0])
+                angle_rad = abs(math.atan(m1) - math.atan(m2))
+                angle_deg = angle_rad*180/math.pi
+                #print('angle diff:', abs(angle_deg))
+                if abs(angle_deg)<3 or abs(angle_deg)>177:
+                    flag='hallway'
+    elif l1.geom_type=='MultiLineString' and l2.geom_type=='LineString':
+        l1_geoms=list(l1.geoms)
+        l2=list(l2.coords)
+        for geom in l1_geoms:
+            l1=geom.coords
+            m1 = (l1[1][1]-l1[0][1])/(l1[1][0]-l1[0][0])
+            m2 = (l2[1][1]-l2[0][1])/(l2[1][0]-l2[0][0])
+            angle_rad = abs(math.atan(m1) - math.atan(m2))
+            angle_deg = angle_rad*180/math.pi
+            #print('angle diff:', abs(angle_deg))
+            if abs(angle_deg)<3 or abs(angle_deg)>177:
+                flag='hallway'
+    elif l1.geom_type=='LineString' and l2.geom_type=='MultiLineString':
+        l2_geoms=list(l2.geoms)
+        l1=list(l1.coords)
+        for geom in l2_geoms:
+            l2=geom.coords
+            m1 = (l1[1][1]-l1[0][1])/(l1[1][0]-l1[0][0])
+            m2 = (l2[1][1]-l2[0][1])/(l2[1][0]-l2[0][0])
+            angle_rad = abs(math.atan(m1) - math.atan(m2))
+            angle_deg = angle_rad*180/math.pi
+            #print('angle diff:', abs(angle_deg))
+            if abs(angle_deg)<3 or abs(angle_deg)>177:
+                flag='hallway'
     else:
         l1=list(l1.coords)
-    if l2.geom_type=='MultiLineString':
-        l2=list(l2.geoms[0].coords)
-    else:
         l2=list(l2.coords)
-    m1 = (l1[1][1]-l1[0][1])/(l1[1][0]-l1[0][0])
-    m2 = (l2[1][1]-l2[0][1])/(l2[1][0]-l2[0][0])
-    angle_rad = abs(math.atan(m1) - math.atan(m2))
-    angle_deg = angle_rad*180/math.pi
-    #print('angle diff:', abs(angle_deg))
-    if abs(angle_deg)<3 or abs(angle_deg)>177:
-        return 'hallway'
-    elif abs(angle_deg)>0 and abs(angle_deg)<135:
-        return 'branch'
-    else:
-        return 'undefined'
-with open('tests_utm.geojson', encoding='utf-8') as file:
-    TL=json.loads(file.read())
+        m1 = (l1[1][1]-l1[0][1])/(l1[1][0]-l1[0][0])
+        m2 = (l2[1][1]-l2[0][1])/(l2[1][0]-l2[0][0])
+        angle_rad = abs(math.atan(m1) - math.atan(m2))
+        angle_deg = angle_rad*180/math.pi
+        #print('angle diff:', abs(angle_deg))
+        if abs(angle_deg)<3 or abs(angle_deg)>177:
+            flag='hallway'
+    return flag
+
 
 def init_gpd(TL):
     gdf=gpd.GeoDataFrame.from_features(TL['features'])
@@ -104,32 +137,55 @@ def explode_gpd(gdf):
     .apply(lambda l: [LineString([c1, c2]) for p in l for c1, c2 in zip(p.coords, list(p.coords)[1:])])
     .explode())
     gdf_exploded=line_segs.to_frame().join(gdf.drop(columns="geometry")).reset_index(drop=True)
-    gdf_exploded.rename(columns={'id':'origin_id', 0: 'line'}, inplace=True)
+    # Some mistake in gdf_exploded columns
+    gdf_exploded.rename(columns={'id':'origin_id', 'geometry': 'line'}, inplace=True)
     gdf_exploded['part_id']=gdf_exploded.index.astype(str)
     gdf_exploded=gdf_exploded.set_geometry('line')
     for i, val in gdf_exploded.iterrows():
         part_id=val.part_id
         line=val.line
         origin_id=val.origin_id
+        
         gdf_exploded.iloc[i]={
             'line': line,
             'origin_id': origin_id,
             'part_id': [part_id]
             }
+    gdf_exploded['simple_index']=None
     return gdf_exploded
 def buffers_gpd(gdf, distance):
     gdf['buffer']=gdf.geometry.buffer(distance=distance, cap_style=2, join_style=2)
+    for i, val in gdf.iterrows():
+        line=val.line
+        buffer=val.buffer
+        origin_id=val.origin_id
+        part_id=val.part_id
+        indexed=simple_index(buffer, gdf, i)
+        gdf.at[i, 'line']=line
+        gdf.at[i, 'buffer']=buffer
+        gdf.at[i, 'origin_id']=origin_id
+        gdf.at[i, 'part_id']=part_id
+        gdf.at[i, 'simple_index']=indexed
     return gdf
-#TODO: перенос в пустой гдф
+
+def simple_index(buffer, gdf, i):
+    gdf_slice=gdf[gdf.index!=i]
+    simple_index=dict(buffer.intersects(gdf_slice['buffer']))
+    for x in [x for x in simple_index.keys() if simple_index[x] == False]:
+        simple_index.pop(x)
+    indexed=list(simple_index.keys())
+    return indexed
+#TODO: due to spatialindex implementation some segments not correct. need more tests with DE-9IM
+
 def process_parts(gdf):
-    #print(gdf)
+    l=len(gdf)
     for i, val1 in gdf.iterrows():
-        #print(i)
+        printProgressBar(i + 1, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
         l1=val1.line
         l1_buff=val1.buffer
+        indexed=val1.simple_index
         gdf_slice=gdf.copy()
-        gdf_slice=gdf_slice[gdf_slice.index!=i]
-        remove_candidates=[]
+        gdf_slice=gdf_slice[gdf_slice.index.isin(indexed)]
         val1_part_id=val1.part_id
         while len(gdf_slice)>0:
             val2=gdf_slice.iloc[-1]
@@ -140,7 +196,6 @@ def process_parts(gdf):
             
             restr=Restrictions(l1, l1_buff, l2, l2_buff)
             flag=restr.check()
-            #print(val1.part_id, val2.part_id, flag)
             if flag=='stream':
                 if isinstance(val1_part_id, str) and isinstance(val2.part_id, str):
                     part_ids=[val1_part_id, val2.part_id]
@@ -150,46 +205,51 @@ def process_parts(gdf):
                     part_ids=[*val1_part_id, val2.part_id]
                 elif isinstance(val1_part_id, list) and isinstance(val2.part_id, list):
                     part_ids=[*val1_part_id, *val2.part_id]
-                #gdf.drop(index=val2_index, inplace=True)
-                
-                #print('in while', val1_part_id, val2.part_id)
-                #print('ids', i, val2_index)
-                #print(part_ids)
-                gdf.at[i, 'line']=restr.to_multiline(flag)
-                gdf.at[i, 'buffer']=normalize(unary_union([l1_buff, l2_buff]))
-                gdf.at[i, 'origin_id']=int(val1.origin_id)
-                gdf.at[i, 'part_id']=part_ids
                 l1=restr.to_multiline(flag)
                 l1_buff=normalize(unary_union([l1_buff, l2_buff]))
                 val1_part_id=part_ids
-                
-                #remove_candidates.append(val2_index[0])
-                '''gdf.at[i]={
-                    'line': restr.to_multiline(flag), 
-                    'buffer': normalize(unary_union([l1_buff, l2_buff])),
-                    'origin_id': val1.origin_id,
-                    'part_id': part_ids
-                    }'''
-                #print(val2_index)
                 gdf=gdf[gdf.line!=val2.line]
-                print(gdf)
-                #print(remove_candidates)
-        #if len(remove_candidates)>0:
-            #print(len(remove_candidates), i, remove_candidates)
-        #    gdf=gdf[~gdf.index.isin(remove_candidates)]
+                gdf.at[i, 'line']=l1
+                gdf.at[i, 'buffer']=l1_buff
+                gdf.at[i, 'origin_id']=int(val1.origin_id)
+                gdf.at[i, 'part_id']=val1_part_id
+                gdf.at[i, 'simple_index']=simple_index(l1_buff, gdf, i)
     return gdf
-# список индексов, после каждой итерации добавить индекс val2 
 
 #TODO: remove duplicates
 #origin is the object that has the biggest length of part_id
 #duplicates are the objects that contain items from origin part_id
+
+#is it significant to use?
 def remove_duplicates(gdf):
     for i, val1 in gdf.iterrows():
         parts=val1.part_id
         if isinstance(parts, list):
             gdf.drop(index=gdf[gdf.part_id.isin(parts)].index, inplace=True)
     return gdf
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
 
+with open('tests_utm2.geojson', encoding='utf-8') as file:
+    TL=json.loads(file.read())
 gdf_TL=init_gpd(TL)
 gdf_exploded_TL=explode_gpd(gdf_TL)
 #print(gdf_exploded_TL.columns)
@@ -200,8 +260,9 @@ gdf_buffer_TL=buffers_gpd(gdf_exploded_TL, 100)
 #exploded.to_file(filename='exploded.gpkg', driver='GPKG')
 
 #! NUMBERS OF PARTS IN MULTILINESTRING SET FROM LEFT TO RIGHT 
-
+# maybe not
 gdf_processed=process_parts(gdf_buffer_TL)
+gdf_processed.drop('simple_index', axis=1, inplace=True)
 gdf_processed_nodupl=remove_duplicates(gdf_processed)
 gdf_processed_nodupl_buffer=gdf_processed_nodupl.copy()
 # geopandas can't export to file with list type of column
@@ -218,3 +279,6 @@ gdf_processed_nodupl_buffer.set_geometry('buffer', inplace=True)
 gdf_processed_nodupl_buffer.drop('line', axis=1, inplace=True)
 gdf_processed_nodupl_buffer.set_crs(epsg=32635, inplace=True)
 gdf_processed_nodupl_buffer.to_file(filename='tests_buffer.gpkg', driver='GPKG')
+
+
+# After exploding, for each LineString buffer find indexes of intersected LineString buffers
